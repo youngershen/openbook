4ac828e6af285f5a75cebddeef59f08f88bf7ee4
Shen Yangang <younger.x.shen@gmail.com>
2015-01-15 08:47:22 +0800

    session design

---
+++ openbook/core/session/tests.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+# PROJECT_NAME : openbook
+# FILE_NAME    : 
+# AUTHOR       : younger shen
---
+++ openbook/core/session/utils.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# PROJECT_NAME : openbook
+# FILE_NAME    : 
+# AUTHOR       : younger shen
+
+
+def generate_session_hash():
+    pass
